54522ae40ad49bb71d09192b0c8bbe5486aeb750
davep684 <dpalma@techlaunch.io>
2018-06-06 19:22:04 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-const api_url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${}&format=json&callback=?`
-const wiki_link = 'https://en.wikipedia.org/wiki'
-const randomEndpoint = '/Special:Random'
+
+
+const search = document.querySelector(".search")
+const searchTerm = document.querySelector(".searchTerm")
+const output = document.querySelector(".output")
+
+function newsSearch(){
+const api_url = (`https://newsapi.org/v2/everything?q=${searchTerm.value}&apiKey=0573d17377384d7ba3d993e2f5b9f927`)
+    $.ajax({
+        type:'GET',
+        url: api_url,
+        dataType:'json',
+        success: (data)=>{
+            console.log(data)
+            output.innerHTML = ""
+            for(let i=0; i<data.articles.length; i++){
+            output.innerHTML += `
+                <li>
+                <p>${data.articles[i].title}</p>
+                <img src="${data.articles[i].urlToImage}" alt = "Img">
+                <a href = "${data.articles[i].url}">${data.articles[i].url}</a>
+                <p>${data.articles[i].source.name}</p>
+                <p>${data.articles[i].description}</p>
+                </li>
+            `
+            }
+        },
+        error: (error)=>{
+            console.log("There was an error")
+        
+        }
+    })
+}
+search.addEventListener('click', newsSearch)
+
+   
